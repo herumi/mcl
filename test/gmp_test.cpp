a972724e7f72e98f8615f9a5d355e1d543e68eb5
MITSUNARI Shigeo <herumi@nifty.com>
2016-04-19 16:14:38 +0900

    add test of gmp

---
+++ test/gmp_test.cpp
@@ -0,0 +1,23 @@
+#include <mcl/gmp_util.hpp>
+#include <vector>
+#include <cybozu/test.hpp>
+
+CYBOZU_TEST_AUTO(testBit)
+{
+	const size_t maxBit = 100;
+	const size_t tbl[] = {
+		3, 9, 5, 10, 50, maxBit
+	};
+	mpz_class a;
+	std::vector<bool> b(maxBit + 1);
+	for (size_t i = 0; i < CYBOZU_NUM_OF_ARRAY(tbl); i++) {
+		a |= mpz_class(1) << tbl[i];
+		b[tbl[i]] = 1;
+	}
+	for (size_t i = 0; i <= maxBit; i++) {
+		bool c1 = mcl::gmp::testBit(a, i);
+		bool c2 = b[i] != 0;
+		CYBOZU_TEST_EQUAL(c1, c2);
+	}
+}
+
