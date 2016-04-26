fef84d997f835b5eb00926d93db87179e6bf4842
MITSUNARI Shigeo <herumi@nifty.com>
2016-04-26 14:34:52 +0900

    add DEBUG option

--- common.mk
+++ common.mk
@@ -22,22 +22,24 @@ MKDIR=mkdir -p
 RM=rm -rf
 
 
-CFLAGS_OPT+=-fomit-frame-pointer -DNDEBUG
-ifeq ($(CXX),clang++)
-  CFLAGS_OPT+=-O3
-else
-  ifeq ($(shell expr $(GCC_VER) \> 4.6.0),1)
-    CFLAGS_OPT+=-Ofast
-  else
+ifneq ($(DEBUG),1)
+  CFLAGS_OPT+=-fomit-frame-pointer -DNDEBUG
+  ifeq ($(CXX),clang++)
     CFLAGS_OPT+=-O3
+  else
+    ifeq ($(shell expr $(GCC_VER) \> 4.6.0),1)
+      CFLAGS_OPT+=-Ofast
+    else
+      CFLAGS_OPT+=-O3
+    endif
   endif
-endif
-ifeq ($(MARCH),)
-ifeq ($(shell expr $(GCC_VER) \> 4.2.1),1)
-  CFLAGS_OPT+=-march=native
-endif
-else
-  CFLAGS_OPT+=$(MARCH)
+  ifeq ($(MARCH),)
+  ifeq ($(shell expr $(GCC_VER) \> 4.2.1),1)
+    CFLAGS_OPT+=-march=native
+  endif
+  else
+    CFLAGS_OPT+=$(MARCH)
+  endif
 endif
 CFLAGS_WARN=-Wall -Wextra -Wformat=2 -Wcast-qual -Wcast-align -Wwrite-strings -Wfloat-equal -Wpointer-arith
 CFLAGS+= -g3
