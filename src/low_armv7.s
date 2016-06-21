ed102088d4f50d7217356089b967d464548aef83
MITSUNARI Shigeo <herumi@nifty.com>
2016-06-21 16:41:56 +0900

    bx lr is faster than mov pc, lr

--- src/low_armv7.s
+++ src/low_armv7.s
@@ -17,9 +17,9 @@ mcl_fp_addNC64:
 # slow
 	.align 2
 mcl_fp_addNC64_2:
-	ldm	r1, {r3, r12}
-	ldm	r2, {r1, r2}
+	ldm		r1, {r3, r12}
+	ldm		r2, {r1, r2}
 	adds	r1, r1, r3
-	adc	r2, r2, r12
-	stm	r0, {r1, r2}
-	mov	pc, lr
+	adc		r2, r2, r12
+	stm		r0, {r1, r2}
+	bx lr
