e22bf8eaf65f3c741049b0b0cd63fe51ee3ba4f6
MITSUNARI Shigeo <herumi@nifty.com>
2016-06-21 17:51:49 +0900

    mcl_fp_addNC96

--- src/low_armv7.s
+++ src/low_armv7.s
@@ -1,6 +1,8 @@
 	.arch armv7-a
 
 	.global mcl_fp_addNC64
+	.global mcl_fp_addNC96_1
+	.global mcl_fp_addNC96_2
 
 	.align 2
 mcl_fp_addNC64:
@@ -9,4 +11,37 @@ mcl_fp_addNC64:
 	adds	r1, r1, r3
 	adc		r2, r2, r12
 	stm		r0, {r1, r2}
+	bx		lr
+
+
+	.align 2
+mcl_fp_addNC96_1:
+	push	{r4, lr}
+	ldm	r1, {r3, r12, lr}
+	ldm	r2, {r1, r4}
+	ldr	r2, [r2, #8]
+	adds	r1, r1, r3
+	adcs	r3, r4, r12
+	adc	r2, r2, lr
+	stm	r0, {r1, r3}
+	str	r2, [r0, #8]
+	pop	{r4, lr}
+	bx		lr
+
+	.align 2
+mcl_fp_addNC96_2:
+	ldr		r3, [r1]
+	ldr		r12, [r2]
+	adds	r3, r3, r12
+	str		r3, [r0]
+
+	ldr		r3, [r1, #4]
+	ldr		r12, [r2, #4]
+	adcs	r3, r3, r12
+	str		r3, [r0, #4]
+
+	ldr		r3, [r1, #8]
+	ldr		r12, [r2, #8]
+	adcs	r3, r3, r12
+	str		r3, [r0, #8]
 	bx		lr
--- test/low_test.cpp
+++ test/low_test.cpp
@@ -8,8 +8,10 @@
 cybozu::XorShift rg;
 
 extern "C" void mcl_fp_addNC64(uint32_t *z, const uint32_t *x, const uint32_t *y);
+extern "C" void mcl_fp_addNC96_1(uint32_t *z, const uint32_t *x, const uint32_t *y);
+extern "C" void mcl_fp_addNC96_2(uint32_t *z, const uint32_t *x, const uint32_t *y);
 
-CYBOZU_TEST_AUTO(addNC)
+CYBOZU_TEST_AUTO(addNC64)
 {
 	using namespace mcl::fp;
 	const size_t N = 64 / UnitBitSize;
@@ -23,6 +25,25 @@ CYBOZU_TEST_AUTO(addNC)
 		mcl_fp_addNC64(w, x, y);
 		CYBOZU_TEST_EQUAL_ARRAY(z, w, N);
 	}
-	CYBOZU_BENCH("add1", mcl_fp_addNC64, x, x, y);
+	CYBOZU_BENCH("add64", mcl_fp_addNC64, x, x, y);
+}
+CYBOZU_TEST_AUTO(addNC)
+{
+	using namespace mcl::fp;
+	const size_t N = 96 / UnitBitSize;
+	Unit x[N], y[N];
+	for (int i = 0; i < 10; i++) {
+		Unit z[N];
+		Unit w[N];
+		rg.read(x, N);
+		rg.read(y, N);
+		low_add<N>(z, x, y);
+		mcl_fp_addNC96_1(w, x, y);
+		CYBOZU_TEST_EQUAL_ARRAY(z, w, N);
+		mcl_fp_addNC96_2(w, x, y);
+		CYBOZU_TEST_EQUAL_ARRAY(z, w, N);
+	}
+	CYBOZU_BENCH("add96_1", mcl_fp_addNC96_1, x, x, y);
+	CYBOZU_BENCH("add96_2", mcl_fp_addNC96_2, x, x, y);
 }
 
