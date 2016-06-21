e76ffe38f5b06b43a6d20ec7e2b3e71dbe768f7a
MITSUNARI Shigeo <herumi@nifty.com>
2016-06-21 11:26:15 +0900

    test mcl_fp_addNC64 for armv7

---
+++ test/low_armv7.s
@@ -0,0 +1,25 @@
+	.arch armv7-a
+
+	.global mcl_fp_addNC64
+	.global mcl_fp_addNC64_2
+
+	.align 2
+mcl_fp_addNC64:
+	ldrd	r2, [r2]
+	stmfd	sp!, {r4, r5}
+	ldrd	r4, [r1]
+	adds	r2, r2, r4
+	adc	r3, r3, r5
+	ldmfd	sp!, {r4, r5}
+	strd	r2, [r0]
+	bx	lr
+
+# slow
+	.align 2
+mcl_fp_addNC64_2:
+	ldm	r1, {r3, r12}
+	ldm	r2, {r1, r2}
+	adds	r1, r1, r3
+	adc	r2, r2, r12
+	stm	r0, {r1, r2}
+	mov	pc, lr
--- test/low_test.cpp
+++ test/low_test.cpp
@@ -3,13 +3,17 @@
 #include <cybozu/xorshift.hpp>
 #include "../src/fp_proto.hpp"
 #include "../src/low_gmp.hpp"
+#include <cybozu/benchmark.hpp>
 
 cybozu::XorShift rg;
+
+extern "C" void mcl_fp_addNC64(uint32_t *z, const uint32_t *x, const uint32_t *y);
+extern "C" void mcl_fp_addNC64_2(uint32_t *z, const uint32_t *x, const uint32_t *y);
 
 CYBOZU_TEST_AUTO(addNC)
 {
 	using namespace mcl::fp;
-	const size_t N = 256 / UnitBitSize;
+	const size_t N = 64 / UnitBitSize;
 	Unit x[N], y[N];
 	for (int i = 0; i < 10; i++) {
 		Unit z[N];
@@ -17,8 +21,14 @@ CYBOZU_TEST_AUTO(addNC)
 		rg.read(x, N);
 		rg.read(y, N);
 		low_add<N>(z, x, y);
-		mcl_fp_addNC256(w, x, y);
+		mcl_fp_addNC64(w, x, y);
+		CYBOZU_TEST_EQUAL_ARRAY(z, w, N);
+		mcl_fp_addNC64_2(w, x, y);
 		CYBOZU_TEST_EQUAL_ARRAY(z, w, N);
 	}
+	CYBOZU_BENCH("add1", mcl_fp_addNC64, x, x, y);
+	CYBOZU_BENCH("add2", mcl_fp_addNC64_2, x, x, y);
+	CYBOZU_BENCH("add1", mcl_fp_addNC64, x, x, y);
+	CYBOZU_BENCH("add2", mcl_fp_addNC64_2, x, x, y);
 }
 
