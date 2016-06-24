ec6ecdeb98d9408b35ab22f663a357eaa8b52e11
MITSUNARI Shigeo <herumi@nifty.com>
2016-06-24 15:28:42 +0900

    use template for benchmark

--- test/low_test.cpp
+++ test/low_test.cpp
@@ -1,6 +1,7 @@
 #define MCL_USE_LLVM
 #include <cybozu/test.hpp>
 #include <cybozu/xorshift.hpp>
+#include <cybozu/itoa.hpp>
 #include "../src/fp_proto.hpp"
 #include "../src/low_gmp.hpp"
 #include <cybozu/benchmark.hpp>
@@ -9,41 +10,44 @@ cybozu::XorShift rg;
 
 extern "C" void mcl_fp_addNC64(uint32_t *z, const uint32_t *x, const uint32_t *y);
 extern "C" void mcl_fp_addNC96(uint32_t *z, const uint32_t *x, const uint32_t *y);
-extern "C" void mcl_fp_addNC96_2(uint32_t *z, const uint32_t *x, const uint32_t *y);
+extern "C" void mcl_fp_addNC128(uint32_t *z, const uint32_t *x, const uint32_t *y);
+
+template<size_t N>
+void addNC(uint32_t *z, const uint32_t *x, const uint32_t *y);
+
+#define DEF_ADD(BIT) template<> void addNC<BIT>(uint32_t *z, const uint32_t *x, const uint32_t *y) { mcl_fp_addNC ## BIT(z, x, y); }
+
+DEF_ADD(64)
+DEF_ADD(96)
+//DEF_ADD(128)
+
+#define CAT(S, BIT) "S##BIT"
+
+template<size_t bit>
+void benchAdd()
+{
+	using namespace mcl::fp;
+	const size_t N = bit / UnitBitSize;
+	Unit x[N], y[N];
+	for (int i = 0; i < 10; i++) {
+		Unit z[N];
+		Unit w[N];
+		rg.read(x, N);
+		rg.read(y, N);
+		low_add<N>(z, x, y);
+		addNC<bit>(w, x, y);
+		CYBOZU_TEST_EQUAL_ARRAY(z, w, N);
+	}
+	std::string name = "name" + cybozu::itoa(bit);
+	CYBOZU_BENCH(name.c_str(), addNC<bit>, x, x, y);
+}
 
 CYBOZU_TEST_AUTO(addNC64)
 {
-	using namespace mcl::fp;
-	const size_t N = 64 / UnitBitSize;
-	Unit x[N], y[N];
-	for (int i = 0; i < 10; i++) {
-		Unit z[N];
-		Unit w[N];
-		rg.read(x, N);
-		rg.read(y, N);
-		low_add<N>(z, x, y);
-		mcl_fp_addNC64(w, x, y);
-		CYBOZU_TEST_EQUAL_ARRAY(z, w, N);
-	}
-	CYBOZU_BENCH("add64", mcl_fp_addNC64, x, x, y);
+	benchAdd<64>();
 }
-CYBOZU_TEST_AUTO(addNC)
+CYBOZU_TEST_AUTO(addNC96)
 {
-	using namespace mcl::fp;
-	const size_t N = 96 / UnitBitSize;
-	Unit x[N], y[N];
-	for (int i = 0; i < 10; i++) {
-		Unit z[N];
-		Unit w[N];
-		rg.read(x, N);
-		rg.read(y, N);
-		low_add<N>(z, x, y);
-		mcl_fp_addNC96(w, x, y);
-		CYBOZU_TEST_EQUAL_ARRAY(z, w, N);
-		mcl_fp_addNC96_2(w, x, y);
-		CYBOZU_TEST_EQUAL_ARRAY(z, w, N);
-	}
-	CYBOZU_BENCH("add96", mcl_fp_addNC96, x, x, y);
-	CYBOZU_BENCH("add96_2", mcl_fp_addNC96_2, x, x, y);
+	benchAdd<96>();
 }
 
