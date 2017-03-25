a0402e567bf6a8e33c8e21b780251cdaa7027884
MITSUNARI Shigeo <herumi@nifty.com>
2017-03-25 11:41:14 +0900

    add option for bn384_test

--- test/bn384_test.cpp
+++ test/bn384_test.cpp
@@ -1,13 +1,17 @@
+#define CYBOZU_TEST_DISABLE_AUTO_RUN
 #include <cybozu/test.hpp>
 #include <cybozu/benchmark.hpp>
+#include <cybozu/option.hpp>
 #include <mcl/bn384.hpp>
 #include <mcl/bn.hpp>
 
 using namespace mcl::bn384;
 
+mcl::fp::Mode g_mode;
+
 void testCurve(const mcl::bn::CurveParam& cp)
 {
-	bn384init(cp);
+	bn384init(cp, g_mode);
 	G1 P;
 	G2 Q;
 	BN::mapToG1(P, 1);
@@ -34,4 +38,21 @@ CYBOZU_TEST_AUTO(pairing)
 	testCurve(mcl::bn::CurveFp382_2);
 	// support 256-bit pairing
 	testCurve(mcl::bn::CurveFp254BNb);
+}
+
+int main(int argc, char *argv[])
+	try
+{
+	cybozu::Option opt;
+	std::string mode;
+	opt.appendOpt(&mode, "auto", "m", ": mode(gmp/gmp_mont/llvm/llvm_mont/xbyak)");
+	if (!opt.parse(argc, argv)) {
+		opt.usage();
+		return 1;
+	}
+	g_mode = mcl::fp::StrToMode(mode);
+	return cybozu::test::autoRun.run(argc, argv);
+} catch (std::exception& e) {
+	printf("ERR %s\n", e.what());
+	return 1;
 }
