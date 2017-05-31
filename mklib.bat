d480d44fc0ef00324b2ec7e71510552924757dd8
MITSUNARI Shigeo <herumi@nifty.com>
2017-05-31 18:54:09 +0900

    fix mklib.bat

--- mklib.bat
+++ mklib.bat
@@ -8,7 +8,7 @@ cl /c %CFLAGS% src\fp.cpp /Foobj\fp.obj
 echo lib /nologo /OUT:lib\mcl.lib /nodefaultlib obj\fp.obj
 lib /nologo /OUT:lib\mcl.lib /nodefaultlib obj\fp.obj
 
-echo cl /c %CFLAGS% src\bn_if.cpp /Foobj\bn_if256.obj /DBLS_MAX_OP_UNIT_SIZE=4
-cl /c %CFLAGS% src\bn_if.cpp /Foobj\bn_if256.obj /DBLS_MAX_OP_UNIT_SIZE=4
+echo cl /c %CFLAGS% src\bn_if.cpp /Foobj\bn_if256.obj /DBN_MAX_FP_UNIT_SIZE=4
+cl /c %CFLAGS% src\bn_if.cpp /Foobj\bn_if256.obj /DBN_MAX_FP_UNIT_SIZE=4
 echo link /nologo /DLL /OUT:bin\bn_if256.dll obj\bn_if256.obj obj\fp.obj %LDFLAGS% /implib:lib\bn_if256.lib
 link /nologo /DLL /OUT:bin\bn_if256.dll obj\bn_if256.obj obj\fp.obj %LDFLAGS% /implib:lib\bn_if256.lib
