f9582713dc827677d6ecfb0d49d884d2120b779f
MITSUNARI Shigeo <herumi@nifty.com>
2017-06-05 07:01:49 +0900

    sample of pairing

--- ffi/go/mcl/mcl_test.go
+++ ffi/go/mcl/mcl_test.go
@@ -2,9 +2,26 @@ package mcl
 
 import "testing"
 
-func testFr(t *testing.T) {
+func testPairing(t *testing.T) {
 	err := Init(CurveFp254BNb)
 	if err != nil {
 		t.Error(err)
+	}
+	var a, b, ab Fr
+	a.SetString("12345678901", 10)
+	b.SetString("abcdef0abcd", 16)
+	FrMul(&ab, &a, &b)
+	var P, aP G1
+	var Q, bQ G2
+	P.HashAndMapTo([]byte("this"))
+	G1Mul(&aP, &P, &a)
+	Q.HashAndMapTo([]byte("that"))
+	G2Mul(&bQ, &Q, &b)
+	var e1, e2 GT
+	Pairing(&e1, &P, &Q)
+	Pairing(&e2, &aP, &bQ)
+	GTPow(&e2, &e2, &ab)
+	if !e1.IsEqual(&e2) {
+		t.Error("not equal pairing")
 	}
 }
