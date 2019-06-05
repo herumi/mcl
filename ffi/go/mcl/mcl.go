32325bf1263c60c6fadc3f3071a962c871e4f6ed
MITSUNARI Shigeo <herumi@nifty.com>
2019-06-05 21:23:19 +0900

    [cgo] separate init.go from mcl.go

--- ffi/go/mcl/mcl.go
+++ ffi/go/mcl/mcl.go
@@ -4,9 +4,6 @@ package mcl
 #cgo bn256 CFLAGS:-DMCLBN_FP_UNIT_SIZE=4
 #cgo bn384 CFLAGS:-DMCLBN_FP_UNIT_SIZE=6
 #cgo bn384_256 CFLAGS:-DMCLBN_FP_UNIT_SIZE=6 -DMCLBN_FR_UNIT_SIZE=4
-#cgo bn256 LDFLAGS:-lmclbn256 -lmcl
-#cgo bn384 LDFLAGS:-lmclbn384 -lmcl
-#cgo bn384_256 LDFLAGS:-lmclbn384_256 -lmcl
 #include <mcl/bn.h>
 */
 import "C"
@@ -27,17 +24,6 @@ const BLS12_381 = C.MCL_BLS12_381
 
 // IoSerializeHexStr
 const IoSerializeHexStr = C.MCLBN_IO_SERIALIZE_HEX_STR
-
-// Init --
-// call this function before calling all the other operations
-// this function is not thread safe
-func Init(curve int) error {
-	err := C.mclBn_init(C.int(curve), C.MCLBN_COMPILED_TIME_VAR)
-	if err != 0 {
-		return fmt.Errorf("ERR mclBn_init curve=%d", curve)
-	}
-	return nil
-}
 
 // GetFrUnitSize() --
 func GetFrUnitSize() int {
