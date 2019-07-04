d403ab84c63ad8f8dc8aa5d77712aaa6153bb025
MITSUNARI Shigeo <herumi@nifty.com>
2019-07-04 09:31:32 +0900

    [java] support windows

--- ffi/java/make_wrap.bat
+++ ffi/java/make_wrap.bat
@@ -5,9 +5,11 @@ set SWIG=..\..\..\..\p\swig\swig.exe
 set PACKAGE_NAME=com.herumi.mcl
 set PACKAGE_DIR=%PACKAGE_NAME:.=\%
 if /i "%1"=="" (
-	set NAME=elgamal
+	set NAME=mcl
+	set LIBNAME=
 ) else (
 	set NAME=%1
+	set LIBNAME=%NAME%
 )
 
 echo [[run swig]]
@@ -15,7 +17,7 @@ mkdir %PACKAGE_DIR%
 set TOP_DIR=../..
 %SWIG% -java -package %PACKAGE_NAME% -outdir %PACKAGE_DIR% -c++ -Wall %NAME%.i
 echo [[make dll]]
-cl /MT /DNOMINMAX /LD /Ox /DNDEBUG /EHsc %NAME%_wrap.cxx %TOP_DIR%/src/fp.cpp -DMCL_NO_AUTOLINK -I%JAVA_INCLUDE% -I%JAVA_INCLUDE%\win32 -I%TOP_DIR%/include -I%TOP_DIR%/../cybozulib/include -I%TOP_DIR%/../cybozulib_ext/include -I%TOP_DIR%/../xbyak /link /LIBPATH:%TOP_DIR%/../cybozulib_ext/lib /OUT:%TOP_DIR%/bin/mcl_%NAME%.dll
+cl /MT /DNOMINMAX /LD /Ox /DNDEBUG /EHsc %NAME%_wrap.cxx %TOP_DIR%/src/fp.cpp -DMCL_NO_AUTOLINK -I%JAVA_INCLUDE% -I%JAVA_INCLUDE%\win32 -I%TOP_DIR%/include -I%TOP_DIR%/../cybozulib/include -I%TOP_DIR%/../cybozulib_ext/include -I%TOP_DIR%/../xbyak /link /LIBPATH:%TOP_DIR%/../cybozulib_ext/lib /OUT:%TOP_DIR%/bin/mcl%LIBNAME%java.dll
 
 call run-%NAME%.bat
 
