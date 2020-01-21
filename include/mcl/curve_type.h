b8fcf48e3bd5858d6a36d03ddd90bc6abe70b069
MITSUNARI Shigeo <herumi@nifty.com>
2020-01-21 15:13:16 +0900

    update comments for MAP_TO_MODE

--- include/mcl/curve_type.h
+++ include/mcl/curve_type.h
@@ -39,6 +39,6 @@ enum {
 enum {
 	MCL_MAP_TO_MODE_ORIGINAL, // see MapTo::calcBN
 	MCL_MAP_TO_MODE_TRY_AND_INC, // try-and-incremental-x
-	MCL_MAP_TO_MODE_ETH2, // eth2.0 spec
-	MCL_MAP_TO_MODE_WB19
+	MCL_MAP_TO_MODE_ETH2, // old eth2.0 spec
+	MCL_MAP_TO_MODE_WB19 // used in new eth2.0 spec
 };
