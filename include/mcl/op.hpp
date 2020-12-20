700751ed0d25f35afdfeefde5fa7531221bd74b6
MITSUNARI Shigeo <herumi@nifty.com>
2020-12-20 10:13:42 +0900

    v1.29

--- include/mcl/op.hpp
+++ include/mcl/op.hpp
@@ -26,7 +26,7 @@
 
 namespace mcl {
 
-static const int version = 0x128; /* 0xABC = A.BC */
+static const int version = 0x129; /* 0xABC = A.BC */
 
 /*
 	specifies available string format mode for X::setIoMode()
