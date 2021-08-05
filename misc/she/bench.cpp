b54ef5229ae0b3902c09d146c4367dde31c6a6e4
MITSUNARI Shigeo <herumi@nifty.com>
2021-08-05 11:43:08 +0900

    [she] refactor

--- misc/she/bench.cpp
+++ misc/she/bench.cpp
@@ -11,6 +11,36 @@ const int maxMsg = 10000;
 
 typedef std::vector<CipherTextG1> CipherTextG1Vec;
 
+void loadSave(const SecretKey& sec, const PublicKey& pub, bool compress)
+{
+	printf("loadSave compress=%d\n", compress);
+	int m = 123;
+	std::string s;
+	{
+		CipherTextG1 c;
+		pub.enc(c, m);
+		std::ostringstream oss;
+		if (compress) {
+			cybozu::save(oss, c);
+		} else {
+			oss.write((const char*)&c, sizeof(c));
+		}
+		s = oss.str();
+		printf("s.size()=%zd\n", s.size());
+	}
+	{
+		std::istringstream iss(s);
+		CipherTextG1 c;
+		if (compress) {
+			cybozu::load(c, iss);
+		} else {
+			iss.read((char*)&c, sizeof(c));
+		}
+		int m2 = sec.dec(c);
+		printf("m2=%d\n", m2);
+	}
+}
+
 void benchEnc(const PrecomputedPublicKey& ppub, int vecN)
 {
 	cybozu::CpuClock clk;
@@ -72,7 +102,7 @@ void benchDec(const PrecomputedPublicKey& ppub, const SecretKey& sec, int vecN)
 	clk.put("dec");
 }
 
-void bench(const std::string& mode, int addN, int vecN)
+void exec(const std::string& mode, int addN, int vecN)
 {
 	SecretKey sec;
 	sec.setByCSPRNG();
@@ -94,43 +124,11 @@ void bench(const std::string& mode, int addN, int vecN)
 		benchDec(ppub, sec, vecN);
 		return;
 	}
+	if (mode == "loadsave") {
+		loadSave(sec, pub, false);
+		loadSave(sec, pub, true);
+	}
 	printf("not supported mode=%s\n", mode.c_str());
-}
-
-void loadSave(bool compress)
-{
-	printf("loadSave compress=%d\n", compress);
-	SecretKey sec;
-	sec.setByCSPRNG();
-
-	PublicKey pub;
-	sec.getPublicKey(pub);
-
-	int m = 123;
-	std::string s;
-	{
-		CipherTextG1 c;
-		pub.enc(c, m);
-		std::ostringstream oss;
-		if (compress) {
-			cybozu::save(oss, c);
-		} else {
-			oss.write((const char*)&c, sizeof(c));
-		}
-		s = oss.str();
-		printf("s.size()=%zd\n", s.size());
-	}
-	{
-		std::istringstream iss(s);
-		CipherTextG1 c;
-		if (compress) {
-			cybozu::load(c, iss);
-		} else {
-			iss.read((char*)&c, sizeof(c));
-		}
-		int m2 = sec.dec(c);
-		printf("m2=%d\n", m2);
-	}
 }
 
 int main(int argc, char *argv[])
@@ -145,8 +143,6 @@ int main(int argc, char *argv[])
 	init();
 	setRangeForDLP(hashSize);
 #endif
-	loadSave(true);
-	loadSave(false);
 
 	cybozu::Option opt;
 	int cpuN;
@@ -156,7 +152,7 @@ int main(int argc, char *argv[])
 	opt.appendOpt(&cpuN, 1, "cpu", "# of cpus");
 	opt.appendOpt(&addN, 128, "add", "# of add");
 	opt.appendOpt(&vecN, 1024, "n", "# of elements");
-	opt.appendParam(&mode, "mode", "enc|add|dec");
+	opt.appendParam(&mode, "mode", "enc|add|dec|loadsave");
 	opt.appendHelp("h");
 	if (opt.parse(argc, argv)) {
 		opt.put();
@@ -165,7 +161,7 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	omp_set_num_threads(cpuN);
-	bench(mode, addN, vecN);
+	exec(mode, addN, vecN);
 } catch (std::exception& e) {
 	printf("ERR %s\n", e.what());
 	return 1;
