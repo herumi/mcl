9c1202c2819014b18c82a586f0eb9ddf1011a3bb
MITSUNARI Shigeo <herumi@nifty.com>
2021-08-05 11:19:25 +0900

    [she] add benchEnc and benchAdd

--- misc/she/bench.cpp
+++ misc/she/bench.cpp
@@ -1,8 +1,76 @@
 #include <mcl/she.hpp>
 #include <cybozu/option.hpp>
+#include <cybozu/benchmark.hpp>
 #include <sstream>
+#include <vector>
+#include <omp.h>
 
 using namespace mcl::she;
+
+
+typedef std::vector<CipherTextG1> CipherTextG1Vec;
+
+void benchEnc(const PrecomputedPublicKey& ppub, int vecN)
+{
+	cybozu::CpuClock clk;
+	CipherTextG1Vec cv;
+	cv.resize(vecN);
+	const int C = 10;
+	for (int k = 0; k < C; k++) {
+		clk.begin();
+		#pragma omp parallel for
+		for (int i = 0; i < vecN; i++) {
+			ppub.enc(cv[i], i);
+		}
+		clk.end();
+	}
+	clk.put("enc");
+}
+
+void benchAdd(const PrecomputedPublicKey& ppub, int addN, int vecN)
+{
+	cybozu::CpuClock clk;
+	CipherTextG1Vec sumv, cv;
+	sumv.resize(vecN);
+	cv.resize(addN);
+	for (int i = 0; i < addN; i++) {
+		ppub.enc(cv[i], i);
+	}
+	const int C = 10;
+	for (int k = 0; k < C; k++) {
+		clk.begin();
+		#pragma omp parallel for
+		for (int j = 0; j < vecN; j++) {
+			sumv[j] = cv[0];
+			for (int i = 1; i < addN; i++) {
+				sumv[j].add(cv[i]);
+			}
+		}
+		clk.end();
+	}
+	clk.put("add");
+}
+
+void bench(const std::string& mode, int addN, int vecN)
+{
+	SecretKey sec;
+	sec.setByCSPRNG();
+
+	PublicKey pub;
+	sec.getPublicKey(pub);
+	PrecomputedPublicKey ppub;
+	ppub.init(pub);
+
+	if (mode == "enc") {
+		benchEnc(ppub, vecN);
+		return;
+	}
+	if (mode == "add") {
+		benchAdd(ppub, addN, vecN);
+		return;
+	}
+	printf("not supported mode=%s\n", mode.c_str());
+}
 
 void loadSave(bool compress)
 {
@@ -54,6 +122,25 @@ int main(int argc, char *argv[])
 #endif
 	loadSave(true);
 	loadSave(false);
+
+	cybozu::Option opt;
+	int cpuN;
+	int addN;
+	int vecN;
+	std::string mode;
+	opt.appendOpt(&cpuN, 1, "cpu", "# of cpus");
+	opt.appendOpt(&addN, 128, "add", "# of add");
+	opt.appendOpt(&vecN, 1024, "n", "# of elements");
+	opt.appendParam(&mode, "mode", "enc|add|dec");
+	opt.appendHelp("h");
+	if (opt.parse(argc, argv)) {
+		opt.put();
+	} else {
+		opt.usage();
+		return 1;
+	}
+	omp_set_num_threads(cpuN);
+	bench(mode, addN, vecN);
 } catch (std::exception& e) {
 	printf("ERR %s\n", e.what());
 	return 1;
