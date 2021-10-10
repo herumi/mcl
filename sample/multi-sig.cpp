4dc2a01e05fb38f892edf13f585a3abbeca3112b
77ph <33835429+77ph@users.noreply.github.com>
2021-10-10 11:38:42 +0300

    Update multi-sig.cpp
    ver 10-Oct-2021 .. test ok

--- sample/multi-sig.cpp
+++ sample/multi-sig.cpp
@@ -1,38 +1,183 @@
-#include <mcl/aggregate_sig.hpp>
-using namespace mcl::aggs;
+#include <mcl/bn256.hpp>
+#include <iostream>
 
-void aggregateTest(const std::vector<std::string>& msgVec)
+using namespace mcl::bn256;
+
+#define MDEBUG 0 // 1 for debug
+
+// H1: M->Zq
+// see https://github.com/herumi/mcl FAQ
+// rewrite x
+template<class F>
+void setHash(F& x, const void *msg, size_t msgSize)
 {
-        const size_t n = msgVec.size();
-        std::vector<SecretKey> secVec(n);
-        std::vector<PublicKey> pubVec(n);
-        std::vector<Signature> sigVec(n);
-        Signature aggSig;
-        bool ok;
-        for (size_t i = 0; i < n; i++) {
-                secVec[i].init();
-                secVec[i].getPublicKey(pubVec[i]);
-                secVec[i].sign(sigVec[i], msgVec[i]);
-                std::cout << "sign " << sigVec[i] << std::endl;
-                ok = pubVec[i].verify(sigVec[i], msgVec[i]);
-                std::cout << "verify " << (ok ? "ok" : "ng") << std::endl;
-        }
-        aggSig.aggregate(sigVec);
-        std::cout << "aggregate sign " << aggSig << std::endl;
-        ok = aggSig.verify(msgVec, pubVec);
-        std::cout << "aggregate verify " << (ok ? "ok" : "ng") << std::endl;
+    uint8_t md[32];
+    mcl::fp::sha256(md, sizeof(md), msg, msgSize);
+    x.setBigEndianMod(md, sizeof(md));
 }
 
 
+// H0: M->G2
+// rewrite P
+void Hash(G1& P, const std::string& m)
+{
+        Fp t;
+        t.setHashOf(m);
+        mapToG1(P, t);
+}
+
+//SkToPk: pub = s * Q 
+//rewrite pub
+void KeyGen(Fr& s, G2& pub, const G2& Q)
+{
+        s.setRand();
+        G2::mul(pub, Q, s); // pub = sQ
+#if MDEBUG
+        std::cout << "secret key " << s << std::endl;
+        std::cout << "public key " << pub << std::endl;
+#endif
+}
+
+
+// Hm = hash_to_point(m)
+// sign = s * Hm
+// rewrite sign
+// https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04
+void CoreSign(G1& sign, const Fr& s, const std::string& m)
+{
+        G1 Hm;
+        Hash(Hm, m);
+        G1::mul(sign, Hm, s); // sign = s H(m)
+}
+
+// ai <- H1(public_key_i,{public_key_1 ... public_key_n})
+// rewrite a
+void genA(std::string allP_serialize, std::string cP_serialize, Fr& a) {
+        std::string hash_tmp_s;
+        hash_tmp_s = cP_serialize + allP_serialize;
+        const char* ptr = (const char*)hash_tmp_s.data();
+        setHash(a,ptr,hash_tmp_s.size());
+} 
+
+//{public_key_1 ... public_key_n}
+//concatenation serialize public_key
+//return std::string 
+std::string sumP(std::vector<G2>& pubKeyVec) {
+        std::string allP;
+        std::string pubKey_s;
+
+        for (size_t i = 0; i < pubKeyVec.size(); i++) {
+                pubKey_s = pubKeyVec[i].getStr(mcl::IoSerialize); // serialize
+                allP = allP + pubKey_s;
+#if MDEBUG
+                printf("pubKey_s data size %d byte\n", (int)pubKey_s.size());
+                printf("allP data size %d byte\n", (int)allP.size());
+#endif
+        } 
+        return allP;
+}
+
+//P = a1×P1 + a2×P2 + a3×P3 + + anxPn
+//rewrite apk, aVec
+void KAg(std::vector<G2>& pubKeyVec, G2& apk, std::vector<Fr>& aVec)
+{
+        const size_t n = pubKeyVec.size();
+        std::string allP = sumP(pubKeyVec); // {pk1 .. pkn}
+        std::string pubKey_s;
+        std::vector<G2> pubKeyTmp(n);
+
+#if MDEBUG
+        std::cout << "size pubKeyVec:" << n << std::endl;
+#endif
+
+        for (size_t i = 0; i < n; i++) {
+#if MDEBUG
+                std::cout << "public key " << pubKeyVec[i] << std::endl;
+#endif
+                pubKey_s = pubKeyVec[i].getStr(mcl::IoSerialize); // serialize
+#if MDEBUG
+                std::cout << "pubKey_s data size "<< (int)pubKey_s.size() << " byte" << std::endl;
+#endif
+                genA(allP,pubKey_s,aVec[i]);
+#if MDEBUG
+                std::cout << "aggregate public key a[" << i << "]:" << aVec[i] << std::endl;
+#endif 
+                G2::mul(pubKeyTmp[i], pubKeyVec[i], aVec[i]); //aixPi
+                G2::add(apk,apk,pubKeyTmp[i]);// P = 0 + a1xP1 ... + anxPn  P+0=P
+        }
+}
+
+//S = a1×S1 + a2×S2 + a3×S3 + + anxSn
+//rewrite multisig
+void MultiSign(std::vector<Fr>& sk, const std::string& m, G1& multisig, std::vector<Fr>& aVec) {
+        const size_t n = sk.size();
+        std::vector<G1> s(n);
+        for (size_t i = 0; i < n; i++) {
+#if MDEBUG
+                std::cout << "multisig a[" << i << "]:" << aVec[i] << std::endl;
+#endif
+                CoreSign(s[i], sk[i], m); // Si
+                G1::mul(s[i],s[i],aVec[i]); // aixSi
+                G1::add(multisig,multisig,s[i]); // S = 0 + a1×S1 + a2×S2 + a3×S3 + + anxSn S+0=S
+        }
+}
+
+//return bool 
+bool CoreVerify(const G1& sign, const G2& Q, const G2& pub, const std::string& m)
+{
+        Fp12 e1, e2;
+        G1 Hm;
+        Hash(Hm, m);
+        pairing(e1, sign, Q); // e1 = e(sign, Q)
+#if MDEBUG
+        std::cout << "e1" << e1 << std::endl;
+#endif
+        pairing(e2, Hm, pub); // e2 = e(Hm, sQ)
+#if MDEBUG
+        std::cout << "e2" << e2 << std::endl; 
+#endif
+        return e1 == e2;
+}
+
+
+//ref: https://eprint.iacr.org/2018/483.pdf
+//ref: https://crypto.stanford.edu/~dabo/pubs/papers/BLSmultisig.html
+// The modified BLS multi-signature construction
 int main(int argc, char *argv[])
 {
-        const std::string msgArray[] = { "abc", "12345", "xyz", "pqr", "aggregate signature" };
-        const size_t n = sizeof(msgArray) / sizeof(msgArray[0]);
-        AGGS::init();
+        std::string m = argc == 1 ? "hello mcl" : argv[1];
+        const size_t n = 1000;
 
-        std::vector<std::string> msgVec(n);
+        // setup parameter
+        initPairing();
+        G2 Q;
+        mapToG2(Q, 1); //g2
+        G2 apk; 
+        G1 multisig;
+        mapToG2(apk, 1);
+        mapToG1(multisig, 1); 
+        G2::sub(apk,apk,apk); // "point at inf" by def P+0=P .. see loop in KAg()
+        G1::sub(multisig,multisig,multisig); // "point at inf" by def P+0=P .. see loop in MultiSign()
+#if MDEBUG
+        std::cout << "apk" << apk << std::endl;
+        std::cout << "multisig" << multisig << std::endl;
+#endif
+
+        bool ok;
+        std::vector<Fr> s(n);
+        std::vector<G2> pubKeyPoint(n);
+        std::vector<Fr> a(n);
+
+        // generate secret key and public key
         for (size_t i = 0; i < n; i++) {
-                msgVec[i] = msgArray[i];
+                KeyGen(s[i], pubKeyPoint[i], Q);
         }
-        aggregateTest(msgVec);
+        KAg(pubKeyPoint,apk,a);
+
+#if MDEBUG
+        std::cout << "aggregate public key " << apk << std::endl;
+#endif
+        MultiSign(s, m, multisig, a);
+        ok = CoreVerify(multisig, Q, apk, m);
+        std::cout << "verify " << (ok ? "ok" : "ng") << std::endl;
 }
