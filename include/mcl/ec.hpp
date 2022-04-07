4ff6c6eef26de0bccd22dbb7caa9b556b0977093
MITSUNARI Shigeo <herumi@nifty.com>
2022-04-07 10:38:24 +0900

    temporary function : mulVec2

--- include/mcl/ec.hpp
+++ include/mcl/ec.hpp
@@ -1865,6 +1865,16 @@ public:
 		}
 		z = r;
 	}
+	template<class tag, size_t maxBitSize, template<class _tag, size_t _maxBitSize>class FpT>
+	static inline void mulVec2(EcT& z, EcT *xVec, const FpT<tag, maxBitSize> *yVec, size_t n)
+	{
+		if (mulVecGLVcore) {
+			mulVecGLVcore(z, xVec, (const void*)yVec, n);
+		} else {
+			puts("ERR");
+			exit(1);
+		}
+	}
 	// multi thread version of mulVec
 	// the num of thread is automatically detected if cpuN = 0
 	template<class tag, size_t maxBitSize, template<class _tag, size_t _maxBitSize>class FpT>
