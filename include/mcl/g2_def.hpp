2187bb98fc0f9bc27609b448b4b2aeca989d24f9
MITSUNARI Shigeo <herumi@nifty.com>
2025-03-13 14:21:05 +0900

    add g2_def.hpp

---
+++ include/mcl/g2_def.hpp
@@ -0,0 +1,24 @@
+#pragma once
+/**
+	@file
+	@brief Define class extension of G2, GT, extension of Fp
+	@author MITSUNARI Shigeo(@herumi)
+	@license modified new BSD license
+	http://opensource.org/licenses/BSD-3-Clause
+*/
+
+#include <mcl/g1_def.hpp>
+#include <mcl/fp_tower.hpp>
+
+namespace mcl {
+
+typedef Fp2T<Fp> Fp2;
+typedef Fp6T<Fp> Fp6;
+typedef Fp12T<Fp, Fr> Fp12;
+typedef Fp12 GT;
+typedef EcT<Fp2, Fr> G2;
+typedef FpDblT<Fp> FpDbl;
+typedef Fp2DblT<Fp> Fp2Dbl;
+typedef Fp6DblT<Fp> Fp6Dbl;
+
+} // mcl
