1f43f12bf75a38619ea1f79f4cd851ebbbc03bf5
MITSUNARI Shigeo <herumi@nifty.com>
2025-03-14 11:33:38 +0900

    tweak

--- test/bls12_test.cpp
+++ test/bls12_test.cpp
@@ -362,6 +362,10 @@ void testSerialize(const G1& P, const G2& Q)
 void testMulVec()
 {
 	puts("testMulVec");
+#ifndef NDEBUG
+	puts("skip in debug");
+	return;
+#endif
 	const size_t n = 8192;
 	cybozu::XorShift rg;
 	std::vector<G1> Pvec(n);
@@ -940,43 +944,6 @@ int main(int argc, char *argv[])
 	printf("static code for BLS12-381\n");
 #else
 	printf("JIT %d\n", mcl::fp::isEnableJIT());
-#endif
-#if 0
-	initPairing(mcl::BLS12_381);
-	cybozu::XorShift rg;
-	const int n = 1;
-	std::vector<Fp> xv(n), yv(n), zv(n);
-	for (int i = 0; i < n; i++) {
-		xv[i].setByCSPRNG(rg);
-		yv[i].setByCSPRNG(rg);
-	}
-	FpDbl dx;
-	FpDbl::mulPre(dx, xv[0], yv[0]);
-	Fp2 x2, y2;
-	x2.a.setByCSPRNG(rg);
-	x2.b.setByCSPRNG(rg);
-	y2.a.setByCSPRNG(rg);
-	y2.b.setByCSPRNG(rg);
-	Fp2Dbl x2d, y2d;
-	Fp2Dbl::mulPre(x2d, x2, x2);
-	Fp2Dbl::mulPre(y2d, x2, y2);
-if(0){
-	puts("----------");
-	xv[0].dump();
-	yv[0].dump();
-	dx.dump();
-	puts("----------");
-//	exit(1);
-}
-//	CYBOZU_BENCH_C("Fp2::neg", 10000000, Fp2::neg, x2, x2);
-	CYBOZU_BENCH_C("Fp2::sqr", 10000000, Fp2::sqr, x2, x2);
-//	CYBOZU_BENCH_C("Fp2::sqrPre", 100000000, Fp2Dbl::sqrPre, x2d, x2);
-//	CYBOZU_BENCH_C("Fp2::mulPre", 100000000, Fp2Dbl::mulPre, x2d, x2, y2);
-//	CYBOZU_BENCH_C("sqrPre", 100000000, FpDbl::sqrPre, dx, xv[0]);
-//	CYBOZU_BENCH_C("mod   ", 100000000, FpDbl::mod, xv[0], dx);
-//	CYBOZU_BENCH_C("mul   ", 100000000, Fp::mul, xv[0], yv[0], xv[0]);
-//	CYBOZU_BENCH_C("sqr   ", 100000000, Fp::sqr, xv[0], xv[0]);
-	return 0;
 #endif
 	return cybozu::test::autoRun.run(argc, argv);
 } catch (std::exception& e) {
