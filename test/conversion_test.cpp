9b2c4778418ab0f08772717c96dba0b6ab9b9914
MITSUNARI Shigeo <herumi@nifty.com>
2025-03-14 15:21:32 +0900

    change test of arrayToDec for mcl::Unit

--- test/conversion_test.cpp
+++ test/conversion_test.cpp
@@ -3,58 +3,39 @@
 
 CYBOZU_TEST_AUTO(arrayToDec)
 {
+	const bool bit64 = MCL_SIZEOF_UNIT == 8;
 	const struct {
-		uint32_t x[6];
+		mcl::Unit x[192 / MCL_UNIT_BIT_SIZE];
 		size_t xn;
 		const char *s;
 	} tbl[] = {
-		{ { 0, 0, 0, 0, 0 }, 1, "0" },
-		{ { 9, 0, 0, 0, 0 }, 1, "9" },
-		{ { 123456789, 0, 0, 0, 0 }, 1, "123456789" },
-		{ { 2147483647, 0, 0, 0, 0 }, 1, "2147483647" },
-		{ { 0xffffffff, 0, 0, 0, 0 }, 1, "4294967295" },
-		{ { 0x540be400, 0x2, 0, 0, 0 }, 2, "10000000000" },
-		{ { 0xffffffff, 0xffffffff, 0, 0, 0 }, 2, "18446744073709551615" },
-		{ { 0x89e80001, 0x8ac72304, 0, 0, 0 }, 2, "10000000000000000001" },
-		{ { 0xc582ca00, 0x8ac72304, 0, 0, 0 }, 2, "10000000001000000000" },
-		{ { 0, 0, 1, 0, 0 }, 3, "18446744073709551616" },
-		{ { 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0 }, 4, "340282366920938463463374607431768211455" },
-		{ { 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff }, 5, "1461501637330902918203684832716283019655932542975" },
-		{ { 0x3b9aca00, 0x5e3f3700, 0x1cbfa532, 0x04f6433a, 0xd83ff078 }, 5, "1234567901234560000000000000000000000001000000000" },
+		{ { MCL_U64_TO_UNIT(0), MCL_U64_TO_UNIT(0), MCL_U64_TO_UNIT(0) }, 1, "0" },
+		{ { MCL_U64_TO_UNIT(9), MCL_U64_TO_UNIT(0), MCL_U64_TO_UNIT(0) }, 1, "9" },
+		{ { MCL_U64_TO_UNIT(123456789), MCL_U64_TO_UNIT(0), MCL_U64_TO_UNIT(0) }, 1, "123456789" },
+		{ { MCL_U64_TO_UNIT(2147483647), MCL_U64_TO_UNIT(0), MCL_U64_TO_UNIT(0) }, 1, "2147483647" },
+		{ { MCL_U64_TO_UNIT(0xffffffff), MCL_U64_TO_UNIT(0), MCL_U64_TO_UNIT(0) }, 1, "4294967295" },
+		{ { MCL_U64_TO_UNIT(0x2540be400), MCL_U64_TO_UNIT(0), MCL_U64_TO_UNIT(0) }, bit64 ? 1 : 2, "10000000000" },
+		{ { MCL_U64_TO_UNIT(0xffffffffffffffff), MCL_U64_TO_UNIT(0), MCL_U64_TO_UNIT(0) }, bit64 ? 1 : 2, "18446744073709551615" },
+		{ { MCL_U64_TO_UNIT(0x8ac7230489e80001), MCL_U64_TO_UNIT(0), MCL_U64_TO_UNIT(0) }, bit64 ? 1 : 2, "10000000000000000001" },
+		{ { MCL_U64_TO_UNIT(0x8ac72304c582ca00), MCL_U64_TO_UNIT(0), MCL_U64_TO_UNIT(0) }, bit64 ? 1 : 2, "10000000001000000000" },
+		{ { MCL_U64_TO_UNIT(0x0), MCL_U64_TO_UNIT(1), MCL_U64_TO_UNIT(0) }, bit64 ? 2 : 3, "18446744073709551616" },
+		{ { MCL_U64_TO_UNIT(0xffffffffffffffff), MCL_U64_TO_UNIT(0xffffffffffffffff), MCL_U64_TO_UNIT(0) }, bit64 ? 2 : 4, "340282366920938463463374607431768211455" },
+		{ { MCL_U64_TO_UNIT(0xffffffffffffffff), MCL_U64_TO_UNIT(0xffffffffffffffff), MCL_U64_TO_UNIT(0xffffffff) }, bit64 ? 3 : 5, "1461501637330902918203684832716283019655932542975" },
+		{ { MCL_U64_TO_UNIT(0x5e3f37003b9aca00), MCL_U64_TO_UNIT(0x04f6433a1cbfa532), MCL_U64_TO_UNIT(0xd83ff078) }, bit64 ? 3 : 5, "1234567901234560000000000000000000000001000000000" },
 	};
 	for (size_t i = 0; i < CYBOZU_NUM_OF_ARRAY(tbl); i++) {
 		const size_t bufSize = 128;
 		char buf[bufSize] = {};
 		const char *str = tbl[i].s;
-		const uint32_t *x = tbl[i].x;
+		const mcl::Unit *x = tbl[i].x;
 		const size_t strLen = strlen(str);
 		size_t n = mcl::fp::arrayToDec(buf, bufSize, x, tbl[i].xn);
 		CYBOZU_TEST_EQUAL(n, strLen);
 		CYBOZU_TEST_EQUAL_ARRAY(buf + bufSize - n, str, n);
 		const size_t maxN = 32;
-		uint32_t xx[maxN] = {};
+		mcl::Unit xx[maxN] = {};
 		size_t xn = mcl::fp::decToArray(xx, maxN, str, strLen);
 		CYBOZU_TEST_EQUAL(xn, tbl[i].xn);
-		CYBOZU_TEST_EQUAL_ARRAY(xx, x, xn);
-	}
-	for (size_t i = 0; i < CYBOZU_NUM_OF_ARRAY(tbl); i++) {
-		const size_t bufSize = 128;
-		char buf[bufSize] = {};
-		const char *str = tbl[i].s;
-		const size_t strLen = strlen(str);
-		uint64_t x[8] = {};
-		size_t xn = (tbl[i].xn + 1) / 2;
-		const uint32_t *src = tbl[i].x;
-		for (size_t i = 0; i < xn; i++) {
-			x[i] = (uint64_t(src[i * 2 + 1]) << 32) | src[i * 2 + 0];
-		}
-		size_t n = mcl::fp::arrayToDec(buf, bufSize, x, xn);
-		CYBOZU_TEST_EQUAL(n, strLen);
-		CYBOZU_TEST_EQUAL_ARRAY(buf + bufSize - n, str, n);
-		const size_t maxN = 32;
-		uint64_t xx[maxN] = {};
-		size_t xxn = mcl::fp::decToArray(xx, maxN, str, strLen);
-		CYBOZU_TEST_EQUAL(xxn, xn);
 		CYBOZU_TEST_EQUAL_ARRAY(xx, x, xn);
 	}
 }
