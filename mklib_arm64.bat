75862e0b45997daa1fc934b6323ed402edf88b37
MITSUNARI Shigeo <herumi@nifty.com>
2025-07-28 17:45:52 +0900

    static version ok

--- mklib_arm64.bat
+++ mklib_arm64.bat
@@ -1,18 +1,20 @@
 @echo off
-
-set CFLAGS=--target=arm64-pc-windows-msvc -O2 -DNDEBUG -I include -I src  -DMCL_SIZEOF_UNIT=8 -DMCL_FP_BIT=384 -DMCL_FR_BIT=256 -DMCL_MSM=0
+call setvar_arm64.bat
 
 if "%1"=="dll" (
   echo make dynamic library DLL
   set LOCAL_CFLAGS=%CFLAGS%
+  set LOCAL_LDFLAGS=%LDFLAGS%
 ) else (
   echo make static library LIB
   set LOCAL_CFLAGS=%CFLAGS% -DMCL_DONT_EXPORT
+  set LOCAL_LDFLAGS=%LDFLAGS%
 )
 
-
-echo %LOCAL_CFLAGS%
 set OBJ=fp.o bint64.o base64.o
+
+echo LOCAL_CFLAGS=%LOCAL_CFLAGS%
+echo LOCAL_LDFLAGS=%LOCAL_LDFLAGS%
 
 clang++ -c src\base64.ll %LOCAL_CFLAGS%
 clang++ -c src\bint64.ll %LOCAL_CFLAGS%
@@ -21,7 +23,7 @@ clang++ -c src\fp.cpp %LOCAL_CFLAGS%
 echo lib /nologo /OUT:lib\mcl.lib /nodefaultlib %OBJ%
 lib /nologo /OUT:lib\mcl.lib /nodefaultlib %OBJ%
 
+
 if "%1"=="dll" (
-     echo link /nologo /DLL /OUT:bin\mclbn.dll %OBJ% %LDFLAGS% /implib:lib\mclbn.lib
-     link /nologo /DLL /OUT:bin\mclbn.dll %OBJ% %LDFLAGS% /implib:lib\mclbn.lib
+clang-cl -target aarch64-pc-windows-msvc /LD %OBJ% /Fe:bin\mclbn.dll lib\mclbn.lib /link "%CLANG_RT_PATH%\clang_rt.builtins-aarch64.lib" msvcrt.lib kernel32.lib /LIBPATH:"%WindowsSdkDir%Lib\%WindowsSDKVersion%\um\arm64" /LIBPATH:"%WindowsSdkDir%Lib\%WindowsSDKVersion%\ucrt\arm64"
 )
