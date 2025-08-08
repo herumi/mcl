dea4cdaad3abd3559223ef51a5a16d90cfb15c50
MITSUNARI Shigeo <herumi@nifty.com>
2025-08-08 16:06:34 +0900

    merge https://github.com/herumi/mcl/pull/229

--- setvar.bat
+++ setvar.bat
@@ -1,2 +1,2 @@
-set CFLAGS=/DNOMINMAX /O2 /DNDEBUG /openmp /W4 /Zi /EHsc /nologo -I./include -I../cybozulib_ext/include -DMCL_SIZEOF_UNIT=8 -DMCL_FP_BIT=384 -DMCL_FR_BIT=256 /DMCL_MSM=1
+set CFLAGS=/DNOMINMAX /O2 /DNDEBUG /openmp /W4 /EHsc /nologo -I./include -I../cybozulib_ext/include -DMCL_SIZEOF_UNIT=8 -DMCL_FP_BIT=384 -DMCL_FR_BIT=256 /DMCL_MSM=1
 set LDFLAGS=/LIBPATH:.\lib /LIBPATH:c:\prog\cybozulib_ext\lib
