4ed636e2ee789137fc3205c8ccfc4d4eafaf4e0d
MITSUNARI Shigeo <herumi@nifty.com>
2025-08-10 12:06:17 +0900

    [skip ci] simplify mklib.bat

--- mklib.bat
+++ mklib.bat
@@ -8,23 +8,13 @@ if "%1"=="dll" (
   set LOCAL_CFLAGS=%CFLAGS% /DMCL_DONT_EXPORT
 )
 
-python3 src\gen_bint_header.py proto > include/mcl/bint_proto.hpp
-python3 src\gen_bint_header.py switch > src/bint_switch.hpp
-
-if 1 == 1 (
-  echo use masm
-  python3 src\gen_bint_x64.py -win -m masm > src\asm\bint-x64-win.asm
-  ml64 -c src\asm\bint-x64-win.asm
-) else (
-  echo use nasm
-  python3 src\gen_bint_x64.py -win -m nasm > src\asm\bint-x64-win.asm
-  nasm -f win64 -o bint-x64-win.obj src\asm\bint-x64-win.asm
-)
 
 echo CFLAGS=%LOCAL_CFLAGS%
 
-set OBJ=obj\fp.obj obj\msm_avx.obj bint-x64-win.obj
+set OBJ=obj\fp.obj obj\msm_avx.obj obj\bint-x64-win.obj
 
+ml64 /c /Foobj\bint-x64-win.obj src\asm\bint-x64-win.asm
+rem nasm -f win64 -o obj\bint-x64-win.obj src\asm\bint-x64-win.asm
 cl /c %LOCAL_CFLAGS% src\fp.cpp /Foobj\fp.obj
 cl /c %LOCAL_CFLAGS% src\msm_avx.cpp /Foobj\msm_avx.obj /arch:AVX512
 
