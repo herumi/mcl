ecf1902ff985a7ab3ab1f6b7b9fbe09cc3593f0e
MITSUNARI Shigeo <herumi@nifty.com>
2026-06-04 15:05:54 +0900

    add stack_c.c for reference

---
+++ src/wasm/stack_c.c
@@ -0,0 +1,22 @@
+// equivalent to stack.s
+// compile with -flto
+
+#include <stdint.h>
+#include <stddef.h>
+
+extern uint8_t *__stack_pointer __attribute__((address_space(1)));
+
+uint8_t *stackSave(void) {
+	return __stack_pointer;
+}
+
+void stackRestore(uint8_t *sp) {
+	__stack_pointer = sp;
+}
+
+uint8_t *stackAlloc(size_t n) {
+	uintptr_t sp = (uintptr_t)__stack_pointer;
+	sp = (sp - n) & ~(uintptr_t)15;
+	__stack_pointer = (uint8_t *)sp;
+	return (uint8_t *)sp;
+}
