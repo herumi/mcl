2d02d64bfd7c8bf5c7d1d9f06a85786362747ac1
MITSUNARI Shigeo <herumi@nifty.com>
2026-09-11 09:28:49 +0900

    add clang=2 option to mklib.bat to build with clang-cl

--- mk.bat
+++ mk.bat
@@ -1,11 +1,21 @@
 @echo off
 call "%~dp0setvar.bat"
+rem set USE_CLANG=2 (mklib clang=2 leaves it) to compile with clang-cl instead of cl
+if "%USE_CLANG%"=="2" (
+  set CXX=clang-cl
+  set LOCAL_CFLAGS=%CLANG_CFLAGS%
+  set LOCAL_LDFLAGS=%LDFLAGS% %CLANG_RT_LIB%
+) else (
+  set CXX=cl
+  set LOCAL_CFLAGS=%CFLAGS%
+  set LOCAL_LDFLAGS=%LDFLAGS%
+)
 if "%1"=="-s" (
   echo use static lib
-  set LOCAL_CFLAGS=%CFLAGS% /DMCL_DONT_EXPORT lib/mcl.lib
+  set LOCAL_CFLAGS=%LOCAL_CFLAGS% /DMCL_DONT_EXPORT lib/mcl.lib
 ) else if "%1"=="-d" (
   echo use dynamic lib
-  set LOCAL_CFLAGS=%CFLAGS% bin/mcl.lib /DMCL_DLL
+  set LOCAL_CFLAGS=%LOCAL_CFLAGS% bin/mcl.lib /DMCL_DLL
 ) else (
   echo "mk (-s|-d) <source file>"
   goto exit
@@ -15,7 +25,7 @@ set EXE=%SRC:.cpp=.exe%
 set EXE=%EXE:.c=.exe%
 set EXE=%EXE:test\=bin\%
 set EXE=%EXE:sample\=bin\%
-echo cl %LOCAL_CFLAGS% %2 %3 /Fe:%EXE% /link %LDFLAGS%
-         cl %LOCAL_CFLAGS% %2 %3 /Fe:%EXE% /link %LDFLAGS%
+echo %CXX% %LOCAL_CFLAGS% %2 %3 /Fe:%EXE% /link %LOCAL_LDFLAGS%
+         %CXX% %LOCAL_CFLAGS% %2 %3 /Fe:%EXE% /link %LOCAL_LDFLAGS%
 
 :exit
--- setvar.bat
+++ setvar.bat
@@ -1,2 +1,7 @@
 set CFLAGS=/DNOMINMAX /O2 /DNDEBUG /openmp /W4 /EHsc /nologo -I./include -I../cybozulib_ext/include -DMCL_SIZEOF_UNIT=8 -DMCL_FP_BIT=384 -DMCL_FR_BIT=256
 set LDFLAGS=/LIBPATH:.\lib /LIBPATH:c:\prog\cybozulib_ext\lib
+rem for clang-cl (USE_CLANG=2) : the same optimization options as Makefile on x64, without /openmp
+set CLANG_CFLAGS=%CFLAGS:/openmp=% /clang:-O3 /clang:-fomit-frame-pointer /clang:-fno-stack-protector -mbmi2 -madx
+rem clang-cl requires clang_rt.builtins for __udivti3 (128-bit division in bint.hpp)
+set CLANG_RT_LIB=
+where clang >nul 2>&1 && for /f "usebackq tokens=*" %%i in (`clang -print-resource-dir`) do set CLANG_RT_LIB="%%i\lib\windows\clang_rt.builtins-x86_64.lib"
